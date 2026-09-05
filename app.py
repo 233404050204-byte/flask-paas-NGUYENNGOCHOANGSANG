6a6933810405b83c4e40b5e6c19dd6eaf3dede67
233404050204-byte <233404050204@ldxh.edu.vn>
2026-09-05 17:19:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def home():
         </style>
     </head>
     <body>
-        <h1>Ứng dụng Flask trên PaaS - phiên bản 2</h1>
+        <h1>Ứng dụng Flask trên PaaS</h1>
 
         <div class="box">
             <p><b>Sinh viên:</b>NGUYEN NGOC HOANG SANG - 233404050204</p>
